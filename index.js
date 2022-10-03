8d73d5712e619059a484145b59e8a1b7a903e341
Argent <jrargent42@gmail.com>
2022-10-03 14:48:52 -0400

    set up inquirer prompt for Employee, test that data is received

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+
+
+
+// function Profile() {
+//     this.employee;
+// };
+
+// Profile.prototype.initializeProfile = function() {
+//     this.employee.push(new Employee());
+//     this.currentEmployee = this.employee[0];
+const promptUser = () => {
+return inquirer.prompt([
+    {
+        type: 'text',
+        name: 'name',
+        message: 'What is the employee name?'
+    },
+    {
+        type: 'text',
+        name: "id",
+        message: 'What is the employee ID?'
+    },
+    {
+        type: 'text',
+        name: 'email',
+        message: 'What is the employee email?'
+    }])
+.then(( { name, id, email }) => {
+    this.employee = new Employee(name, id, email);
+    console.table(this.employee);
+})
+}
+
+promptUser();
+
