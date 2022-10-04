12e0f55601c69dfb0af587265faa5d591b5ce50d
Argent <jrargent42@gmail.com>
2022-10-03 20:02:55 -0400

    set up manager object test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,12 @@
+const Manager = require('../lib/Manager.js');
+
+test('creates a Manager object', () => {
+    const manager = new Manager('Argent', 42, 'jrargent42@gmail.com', 'office#2');
+
+    expect(manager.name).toBe('Argent');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe('jrargent42@gmail.com');
+    expect(manager.officeNumber).toBe('office#2');
+    
+})
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
 
 
 
